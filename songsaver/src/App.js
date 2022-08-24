32defc3ef4a96cae843da239ff953cfde63d2b46
Rianne <riannebultena@gmail.com>
2022-08-24 15:22:30 +0200

    Added Routing

--- songsaver/src/App.js
+++ songsaver/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 
+import { Link, Route, Routes } from "react-router-dom";
+
 import SongOverview from "./components/SongOverview";
+import About from "./pages/About"
 
 function App() {
   return (
     <div className="App">
-      <SongOverview />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li><Link to="/About">About</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<SongOverview />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+      {/* <SongOverview /> */}
     </div>
   )
 }
